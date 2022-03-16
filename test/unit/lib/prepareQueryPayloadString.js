960e8fddc12663f0bf0eef73ee55e3ce77e28a8b
Charles Hill <chill@degreesofzero.com>
2022-03-16 14:45:43 -0500

    Removed underscore
    Added test for prepareQueryPayloadString

--- test/unit/lib/prepareQueryPayloadString.js
+++ test/unit/lib/prepareQueryPayloadString.js
@@ -1,3 +1,18 @@
-describe.skip('prepareQueryPayloadString(query)', function() {
-	// !!!
+const assert = require('assert');
+const { prepareQueryPayloadString } = require('../../../');
+
+describe('prepareQueryPayloadString(query)', function() {
+
+	it('returns payload string, sorted alphabetically by key', function() {
+		const query = {
+			nonce: '6352744c67396aa43ac7',
+			tag: 'withdrawRequest',
+			id: 'b6cb8e81e3',
+			maxWithdrawable: 60000,
+			defaultDescription: '',
+			minWithdrawable: 50000,
+		};
+		const result = prepareQueryPayloadString(query);
+		assert.strictEqual(result, 'defaultDescription=&id=b6cb8e81e3&maxWithdrawable=60000&minWithdrawable=50000&nonce=6352744c67396aa43ac7&tag=withdrawRequest');
+	});
 });
